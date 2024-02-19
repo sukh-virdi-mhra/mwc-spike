855c4fc058c90a3e6e649f99de0125f4eab395ef
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2024-02-19 18:31:56 +0000

    use stylesheet and configure nunjucks

--- app.js
+++ app.js
@@ -3,8 +3,18 @@ const nunjucks = require('nunjucks');
 const path = require('path');
 const app = express();
 
+app.use('/stylesheets', express.static(path.join(dirname, './node_modules/govuk-frontend/dist/govuk')));
+app.use('/javascripts', express.static(path.join(dirname, './node_modules/govuk-frontend/dist/govuk')));
+
+nunjucks.configure([
+  './node_modules/govuk-frontend/dist',
+  './views'
+], {
+  express: app
+});
+
 app.get('/', (request, response) => {
-    response.render('template.njk');
+    response.render('website-checker.njk');
 });
 
 const PORT = process.env.PORT || 3000;
