885fb9e5dea5094d4bd5dbdc3a10d833a82f9c6b
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2024-02-20 18:21:22 +0000

    install dependencies, gitignore connection file and create test db route

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const path = require('path');
+const pool = require('./connection');
+
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/test-db-connection', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT $1::text as message', ['Database connection test successful']);
+    const message = result.rows[0].message;
+    client.release();
+    res.send(`Connection test successful: ${message}`);
+  } catch (err) {
+    console.error('Error connecting to database', err);
+    res.status(500).send('Error connecting to database');
+  }
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/stylesheets', express.static(path.join(__dirname, './node_modules/govuk-frontend/dist/govuk')));
 app.use('/javascripts', express.static(path.join(__dirname, './node_modules/govuk-frontend/dist/govuk')));
@@ -13,11 +32,10 @@ nunjucks.configure([
   express: app
 });
 
-app.get('/', (request, response) => {
-    response.render('question.njk');
+app.get('/', (req, res) => {
+  res.render('question.njk');
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
