1dcd400bcfe482ac0abf3777c4b34184e149509d
Sukh Virdi <sukh.virdi2@mhra.gov.uk>
2024-02-20 18:22:21 +0000

    post submit data into form table

--- app.js
+++ app.js
@@ -36,6 +36,21 @@ app.get('/', (req, res) => {
   res.render('question.njk');
 });
 
+app.post('/submit', async (req, res) => {
+  const { websiteLink } = req.body;
+
+  try {
+    const client = await pool.connect();
+    const result = await client.query('INSERT INTO form_data (website_link) VALUES ($1)', [websiteLink]);
+    client.release();
+    res.send('Form data submitted successfully!');
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).send('An error occurred while processing your request.');
+  }
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
